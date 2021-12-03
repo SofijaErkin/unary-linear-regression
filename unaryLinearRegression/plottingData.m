d65c6d86b582d9cbb0b1141fffa7dee1523d56ff
Vittore Marcas <matthiasxywilliams@gmail.com>
2021-12-03 09:10:36 +0800

    Update plottingData.m
    Minus some empty lines.

--- unaryLinearRegression/plottingData.m
+++ unaryLinearRegression/plottingData.m
@@ -2,7 +2,6 @@ function plottingData(x, y)
 %PLOTDATA Plots the data points x and y into a new figure 
 %   PLOTDATA(x,y) plots the data points and gives the figure axes labels of
 %   population and profit.
- 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Plot the training data into a figure using the 
 %               "figure" and "plot" commands. Set the axes labels using
@@ -13,16 +12,10 @@ function plottingData(x, y)
 % Hint: You can use the 'rx' option with plot to have the markers
 %       appear as red crosses. Furthermore, you can make the
 %       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
- 
- 
 figure('name','Observations'); % open a new figure window
 plot(x,y,'rx','MarkerSize',8); %plot the data
 ylabel('Profit in House in $ 100k');   %set the y-axis label
 xlabel('Population of City in 10k');%set the x-axis label
 title('data_Analytics.txt');
- 
- 
- 
 % ============================================================
- 
 end
