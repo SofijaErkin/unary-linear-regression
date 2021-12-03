e551e512f34ae30bb12a521117b3f6303b53beec
Vittore Marcas <matthiasxywilliams@gmail.com>
2021-12-03 09:12:25 +0800

    Update regression_Predicitive.m
    Minus some empty lines.

--- unaryLinearRegression/regression_Predicitive.m
+++ unaryLinearRegression/regression_Predicitive.m
@@ -3,36 +3,28 @@ fprintf('      Plotting data ...\n')
 data = load('data_Analytics.txt');
 X = data(:, 1); y = data(:, 2);
 m = length(y); % number of training examples
- 
 % Plot Data
 % Note: You have to complete the code in plotData.m
 plottingData(X, y);
- 
 fprintf('Program paused. Press any to continue.\n');
 pause;
-
 %% Gradient descent 
 fprintf('    Running gradient descent ...')
- 
 X = [ones(m, 1), data(:,1)]; % Add a column of ones to x
 theta = zeros(2, 1); % initialize fitting parameters
- 
 % Some gradient descent settings
 iterations = 1500;
 alpha = 0.01;
-
 % compute and display initial cost
 fprintf('        Initial cost')
 computeCost(X, y, theta)
 fprintf('Press any key to continue.\n');
 pause;
-
 % run gradient descent
 theta = gradientDescent(X, y, theta, alpha, iterations);
 disp(['    theta0=',num2str(theta(1)),'   theta1=',num2str(theta(2))]);
 disp('Press any key to continue');
 pause;
-
 hold on; % keep previous plot visible
 plot(X(:,2), X*theta, '-')
 legend('Training data', 'Linear regression')
@@ -40,17 +32,13 @@ fprintf('    Keep previous plot visible...\n')
 hold off % don't overlay any more plots on this figure
 fprintf('Program paused. Press any to continue.\n');
 pause;
-
 %% Visualizing J(theta_0, theta_1) 
 fprintf('Visualizing J(theta_0, theta_1) ...\n')
- 
 % Grid over which we will calculate J
 theta0_vals = linspace(-10, 10, 100);
 theta1_vals = linspace(-1, 4, 100);
- 
 % initialize J_vals to a matrix of 0's
 J_vals = zeros(length(theta0_vals), length(theta1_vals));
- 
 % Fill out J_vals
 for i = 1:length(theta0_vals)
     for j = 1:length(theta1_vals)
@@ -58,7 +46,6 @@ for i = 1:length(theta0_vals)
       J_vals(i,j) = computeCost(X, y, t);
     end
 end
-
 % Because of the way meshgrids work in the surf command, we need to 
 % transpose J_vals before calling surf, or else the axes will be flipped
 J_vals = J_vals';
@@ -69,7 +56,6 @@ surf(theta0_vals, theta1_vals, J_vals)
 xlabel('\theta_0'); ylabel('\theta_1');
 fprintf('Press any key to continue\n');
 pause;
-
 % Contour plot
 fprintf('       Contour plot...\n');
 figure('name','Contour plot');;
@@ -80,7 +66,6 @@ hold on;
 plot(theta(1), theta(2), 'rx', 'MarkerSize', 10, 'LineWidth', 2);
 fprintf('Press any key to continue\n');
 pause;
-
 % Forecasting the urban population with a price of 40,000 and 80,000
 predict1=[1 4]*theta;
 predict2=[1 8]*theta;
