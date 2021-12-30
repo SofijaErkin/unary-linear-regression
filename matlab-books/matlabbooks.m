a76335c5da10b7a47bc16d6bdaa21fdb7ecf8615
SofijaMarcas <matthiasxywilliams@gmail.com>
2021-12-30 11:01:14 +0800

    add directory matlab-books

---
+++ matlab-books/matlabbooks.m
@@ -0,0 +1,13 @@
+% matlab books
+% Digital image processing MATLAB Chinese version
+% unary-linear-regression belongs to one of the son-set of Machine Learning.
+
+% The better way to witness matlab is to read MATLAB and Simulink Based Books.
+% MATLAB and Simulink Based Books: https://www.mathworks.com/academia/books.html
+
+% Or, just use bookauthority.
+% bookauthority for matlab: https://bookauthority.org/books/new-matlab-books.
+
+% Finally, computingforgeeks may help you.
+% matlab via computingforgeeks: 
+% https://computingforgeeks.com/best-books-to-learn-matlab-programming/
